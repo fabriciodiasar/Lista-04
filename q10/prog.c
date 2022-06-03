4847a6d6a04d264828d428b16794f49013f160c1
fabriciodiasar <92959461+fabriciodiasar@users.noreply.github.com>
2022-06-03 18:08:15 -0300

    first commit

--- q10/prog.c
+++ q10/prog.c
@@ -1,91 +1,47 @@
 ﻿// Escreva um programa que gere um vetor X com números pseudoaleatórios no intervalo [0,N − 1]. Crie um vetor F com N posições e armazene em cada posição de F a quantidade de ocorrências dos elementos de X.
 
-
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
-
-
 #define TAM 10
-
 #define N 7
-
-
 
 void imprimirVetor(int *, int);
 
-
-
-int main(){
-  
+int main(){  
 	int x[TAM], f[N], *pv = NULL, *pr = NULL, count;
 
-  
-
-	pv = x;
-  
+	pv = x;  
 	pr = f;
-
-  
-
+	
 	srand(time(0));
-  
-	for(int i = 0; i < TAM; i++)
-    
+  	for(int i = 0; i < TAM; i++)
 		*(pv + i) = rand() % N;
-
-  
-
+	
 	puts("\nVetor X:");
-  
 	imprimirVetor(pv, TAM);
 
-  
-
-	for(int i = 0; i < N; i++){
-    
+	for(int i = 0; i < N; i++){    
 		count = 0;
     
-    
 		for(int j = 0; j < TAM; j++){
-      
 			if (*(pv + j) == i)
-        
-			count += 1;
-    
+        			count += 1;
 		}
     
-    
 		*(pr + i) = count;
-  
 	}
 
-  
-	
-	puts("Vetor F:");
-  
+	puts("Vetor F:");  
 	imprimirVetor(pr, N);
   
-  
-
 	return 0;
-
 }
 
-
-
-void imprimirVetor(int *v, int k){
-  
+void imprimirVetor(int *v, int k){  
 	for(int i = 0; i < k; i++)
-    
 		printf("%d ", *(v + i));
 
-  
-
 	puts("\n");
-
 }
