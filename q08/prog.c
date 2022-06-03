b562a246c5d096c4184f0f1f15de8bde9785b0ed
fabriciodiasar <92959461+fabriciodiasar@users.noreply.github.com>
2022-06-03 18:04:06 -0300

    first commit

--- q08/prog.c
+++ q08/prog.c
@@ -1,80 +1,43 @@
 ﻿// Escreva um programa que busque um caractere fornecido em uma string utilizando ponteiros.
 
-
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
-
 
 #define TAM 30
 
-
-
 int buscaCaractere(char *, char *);
 
-
-
-int main(){
-  
+int main(){  
 	char str[TAM], c, *pv = NULL, *pr = NULL;
 
-  
-
 	pv = str;
-  
 	pr = &c;
 
-  
-
 	puts("\nDigite uma string: ");
-  
-	scanf("%[^\n]s", pv);
+  	scanf("%[^\n]s", pv);
 
-  
-
-	getchar();
-
-  
+ 	getchar();
 
 	puts("\nQual caractere você quer buscar na string? ");
+  	scanf("%c", pr);
   
-	scanf("%c", pr);
-  
-  
-
-	if(buscaCaractere(pv, pr))    
+  	if(buscaCaractere(pv, pr))    
 		puts("\nEsse caractere existe na string!\n");
-  
-	else   
+	else
 		puts("\nEsse caractere NÃO existe na string!\n");
-
 	
-
- return 0;
+ 	return 0;
 
 }
 
-
-
 int buscaCaractere(char *str, char *c) {  
-  
 	int i = 0;
   
-  
-	while (1) {
-    
+	while (1) {  
 		if (*(str + i) == *c) return 1;
-    
-		if (*(str + i) == '\0') break;
-    
-		i++;
-  
+    		if (*(str + i) == '\0') break;
+    		i++;
 	}
-  
-  
 	
 	return 0;
-
 }
