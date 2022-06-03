53bf10996f9da632ce484284fe88b2ee41edb865
fabriciodiasar <92959461+fabriciodiasar@users.noreply.github.com>
2022-06-03 18:00:38 -0300

    first commit

--- q06/prog.c
+++ q06/prog.c
@@ -1,66 +1,34 @@
 // Escreva um programa que copie uma string para outra usando ponteiros.
 
-
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
-
 
 #define TAM 30
 
-
-
 void copiaString(char *, char *);
 
-
-
-int main(){
-  
+int main(){  
 	char str1[TAM], str2[TAM], *pv = NULL, *pr = NULL;
 
-  
-
-	pv = str1;
-  
+	pv = str1;  
 	pr = str2;
-
-  
-
+	
 	puts("\nDigite a string:");
-  
 	scanf("%s", pv);
 
-  
-
 	copiaString(pv, pr);
-
-  
 	
 	printf("\nString 1 = %s\n", str1);
-  
 	printf("String 2 = %s\n\n", str2);
 
-  
-
 	return 0;
-
 }
 
-
-
-void copiaString(char *str1, char *str2){
-    
+void copiaString(char *str1, char *str2){    
 	int i = 0;
   
-    
-	while (*(str1 + i) != '\0'){
-        
+	while (*(str1 + i) != '\0'){      
 		*(str2 + i) = *(str1 + i);
-        
 		i++;
-    
 	}
-
 }
